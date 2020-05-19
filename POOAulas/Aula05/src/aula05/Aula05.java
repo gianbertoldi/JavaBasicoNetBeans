6e6eb4b66e20e29388d4bf89a3a0410fec59d736
Gian Bertoldi <bertoldi555@icloud.com>
2020-05-19 20:09:06 -0300

    fim aula
    cod completo da aula

--- POOAulas/Aula05/src/aula05/Aula05.java
+++ POOAulas/Aula05/src/aula05/Aula05.java
@@ -1,21 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aula05;
 
-/**
- *
- * @author berto
- */
 public class Aula05 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        ContaBanco p1 = new ContaBanco();
+        p1.setNumConta(111);
+        p1.setDono("Jubileu");
+        p1.abrirConta("CC");
+        
+         
+        ContaBanco p2 = new ContaBanco();
+        p2.setNumConta(222);
+        p2.setDono("Creusa");
+        p2.abrirConta("CP");
+        
+        
+        
+        p1.depositar(100);
+        p2.depositar(500);
+        p2.sacar(100);
+        
+        p1.sacar(150);
+        p1.fecharConta();
+        
+        p2.estadoAtual();
+        p1.estadoAtual();
     }
     
 }
--- POOAulas/Aula05/src/aula05/ContaBanco.java
+++ POOAulas/Aula05/src/aula05/ContaBanco.java
@@ -18,6 +18,14 @@ public class ContaBanco {
     private boolean status;
     
     //metodos personalisados 
+    public void estadoAtual(){
+        System.out.println("-------------------------------");
+        System.out.println("Conta: " + this.getNumConta());
+        System.out.println("Tipo: " + this.getTipo());
+        System.out.println("Dono: " + this.getDono());
+        System.out.println("Saldo: " + this.getSaldo());
+        System.out.println("Status: " + this.getStatus());
+    }
     
     public void abrirConta(String t) {
         this.setTipo(t);
@@ -54,12 +62,33 @@ public class ContaBanco {
         
     }
     public void sacar(float v){
-        
+        if (this.getStatus()) {
+            if (this.getSaldo() >= v){
+                this.setSaldo(this.getSaldo() - v);
+                System.out.println("Saque realizado na conta de " + this.getDono());
+            }else {
+                System.out.println("Saldo insufinciente para saque");
+            }
+        }else{
+            System.out.println("imposssivel sacar em uma conta inativa");
+        }
         
         
     }
     public void pagarMensal(){
-        
+        int v = 0;
+        if (this.getTipo() == "CC") {
+            v = 12;
+        } else if(this.getTipo() == "CP") {
+            v = 20;
+        }
+        if (this.getStatus()) {
+            this.setSaldo(this.getSaldo() - v);
+            System.out.println("Mensalidade paga com sucesso pelo " + this.getDono());
+        } else {
+            System.out.println("Impossivel pagar de uma conta fechada!");
+        }
+            
     }
     // metodos especial
     public void ContaBanco(){ //Contructor

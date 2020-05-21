775d60fb27110e47aae7c975a045c34946630153
Gian Bertoldi <bertoldi555@icloud.com>
2020-05-21 11:01:30 -0300

    atualizaçãoBanco
    novo

--- POOAulas/ContaBanco/src/contabanco/ContaBanco.java
+++ POOAulas/ContaBanco/src/contabanco/ContaBanco.java
@@ -10,16 +10,18 @@ public class ContaBanco {
     public static void main(String[] args) {
      ContaCli c1 = new ContaCli();
      
-     String decision = " ";
+     String decision;
      
      JOptionPane.showMessageDialog(null,"Bem vindo a banco 42");
      c1.setConta(JOptionPane.showInputDialog("<html> Deseja abrir uma conta corrente ou popança? <br> [cc/cp]</br></htmal>" )) ;
      c1.setDono(JOptionPane.showInputDialog(null,"<html>Informe seu nome para abrir a conta</html>"));
      c1.getNumConta();
-     decision = (JOptionPane.showInputDialog(null, "Deseja ativar a conta"));
-        if ("s".equals(decision)){
-            int c,n;
+     decision = (JOptionPane.showInputDialog(null, "Deseja ativar a conta" + "\n" + "[s]/[n]"));
+        if ("s".equals(decision) && "S".equals(decision)){
+             int c,n;        
+                    
             do{
+               
                 c = Integer.parseInt(JOptionPane.showInputDialog(null, "<html>Banco 42 <br><em> (Aperte 0 para sair ou 1 para continuar</em></html>"));
 
                 if (c == 0){
@@ -27,6 +29,8 @@ public class ContaBanco {
                 }else{
                     n = Integer.parseInt(JOptionPane.showInputDialog(null,"<html>Digite 1 [Ativar a conta]<br>Digite 2 [Extrato]<br>Digite 3 [Fechar a conta]<br>Digite 4 [Depositar]"
                 + "<br>Digite 5 [Sacar]<br>Digite 6 [Pagar Mensalidade]<br>Digite 0 [SAIR]"));
+                
+                
                     switch(n){
                         case 0:
                             break;
--- POOAulas/ContaBanco/src/contabanco/ContaCli.java
+++ POOAulas/ContaBanco/src/contabanco/ContaCli.java
@@ -21,9 +21,10 @@ public final class ContaCli {
     private float saldo;
     private boolean status;
 
-    public ContaCli() {
-        this.saldo = 0f;
-        this.status = true;
+    public void ContaCli() {
+        this.setSaldo(0);
+        this.status = false;
+        
     }
 
     
@@ -67,9 +68,11 @@ public final class ContaCli {
 
     public void getStatus(){
         if (status = true ) {
-            System.out.println("Conta aberta");
+            JOptionPane.showMessageDialog(null, "A conta do " + dono + " esta aberta!");
+            //("Conta aberta");
         } else {
-            System.out.println("Conta fechada");
+            JOptionPane.showMessageDialog(null,"A conta do " + dono + "Esta fechada!");
+            //System.out.println("Conta fechada");
         }
     }
     
@@ -92,54 +95,76 @@ public final class ContaCli {
         this.status = status;
     }
     
-    public void pagarMensal(){
-        if ("cc".equals(this.getConta())) {
-            this.setSaldo(getSaldo() - 12);
-        } else if ("cp".equals(this.getConta())) {
-            this.setSaldo(getSaldo() - 20);
-        }
-    }
     
     public void abrirConta(){
-        if("cc".equals(this.getConta())){
+        if("cc".equals(this.getConta()) || "CC".equals(this.getConta())){
             this.setSaldo(50);
             this.setStatus(true);
-        } else if("cp".equals(this.getConta())) {
+        } else if("cp".equals(this.getConta()) || "CP".equals(this.getConta())) {
             this.setSaldo(150);
             this.setStatus(true);
         } 
+        JOptionPane.showMessageDialog(null, "Conta aberta com sucesso!");
+    }
+    public void pagarMensal(){
+        int v = 0;
+        
+        if (this.getConta() == "CC" && this.getConta() == "cc") {
+           v=12; 
+        } else if (this.getConta() == "CP" && this.getConta() == "cp") {
+            v=20;
+        }
+        if (this.isStatus()) {
+            this.setSaldo(this.getSaldo() - v);
+            JOptionPane.showMessageDialog(null, "<html>Pagameto da mensalidade no valor de " + v + " na conta de " + dono +" realisado com sucesso!</html>");
+        } else {
+            JOptionPane.showMessageDialog(null, "");
+        }
+        
+        /*if ("cc".equals(this.getConta())) {
+            this.setSaldo(getSaldo() - 12);
+        } else if ("cp".equals(this.getConta())) {
+            this.setSaldo(getSaldo() - 20);
+        }*/
     }
     
+    
     public void fecharConta(){
-        System.out.println("<html>Verifique que sua conta esta sem dinheiro e sem depitos para fechar<html> ");
+        JOptionPane.showMessageDialog(null,"<html>Verifique que sua conta esta sem dinheiro e sem depitos para fechar</html> " );
+        //System.out.println("<html>Verifique que sua conta esta sem dinheiro e sem depitos para fechar<html> ");
         if (this.getSaldo() < 0) {
-            System.out.println("<html>Não pode fechar a conta com o saldo negativo <hatml>" ); 
+            JOptionPane.showMessageDialog(null,"<html>Não pode fechar a conta com o saldo negativo </hatml>" );
+            //System.out.println("<html>Não pode fechar a conta com o saldo negativo <hatml>" ); 
         } else if (this.getSaldo() > 0) {
-            System.out.println("<html>Sua conta deve estar sem saldo para fechar");
+            JOptionPane.showMessageDialog(null,"<html>Sua conta deve estar sem saldo para fechar</html>" );
+            //System.out.println("<html>Sua conta deve estar sem saldo para fechar");
         } else {
-            System.out.println("sua conta esta fechada!!");
+            JOptionPane.showMessageDialog(null, "Sua conta foi fechada com sucesso!");
+            //System.out.println("sua conta esta fechada!!");
         }
     }
     
     public void depositar(float valor){
         if(this.isStatus() == true){
             this.setSaldo(getSaldo() + valor);
+            JOptionPane.showMessageDialog(null, "Deposito de " + valor + "Feito na conta de " + dono);
         }else{
-            System.out.println("Voce não possui uma conta para depositar");
+            JOptionPane.showMessageDialog(null, "É necessario ter uma conta para depositar!");
+            //System.out.println("Voce não possui uma conta para depositar");
         } 
     }
     
     public void sacar(float valor){
-        if (this.isStatus() == true && this.getSaldo() > 0) {
-            this.setSaldo(getSaldo() - valor);
-            if (valor > saldo){
-                System.out.println("Voce passou o limite de seu saldo");
+        if (this.isStatus()) {
+            if (this.getSaldo() >= valor) {
+                this.setSaldo(this.getSaldo() - valor);
+                JOptionPane.showMessageDialog(null,"Saque de " +valor + " na conta de " + dono);
             } else {
-                System.out.println("Saque feito com sucesso!!");
-            }
-            
+                JOptionPane.showMessageDialog(null, "Saldo insufiente!" + "\n" + "Seu saldo é de: " + saldo);
+            }  
         } else {
-            System.out.println("Voce precisa ter um conta ou saldo para fazer o sacar");
+            JOptionPane.showMessageDialog(null, "É preciso ter uma conta ativa para sacar");
+            //System.out.println("Voce precisa ter um conta ou saldo para fazer o sacar");
         }
     }
     

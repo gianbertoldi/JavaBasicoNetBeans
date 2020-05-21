a5b3f26539824f7c7de4c2649764903d958384b3
Gian Bertoldi <bertoldi555@icloud.com>
2020-05-21 19:43:32 -0300

    agora foi
    agora vai

--- POOAulas/Aula07UEF/src/aula07uef/Lutador.java
+++ POOAulas/Aula07UEF/src/aula07uef/Lutador.java
@@ -11,4 +11,134 @@ package aula07uef;
  */
 public class Lutador {
     
+    //atributos 
+    private String nome,nacionalidade,categoria;
+    private int idade,vitorias,derrotas,empates;
+    private float altura,peso;
+    
+    //Metodos publicos 
+    public void apresentar(){
+        System.out.println("----------------------------------------------------");
+        System.out.println("CHEGOU A HORA! Apresentamos o lutador " + this.getNome());
+        System.out.println("Diretamente de " + this.getNacionalidade());
+        System.out.println("Com " + this.getIdade() + " anos e" + this.getAltura() + " de altura");
+        System.out.println("pasando "+ this.getPeso()+"Kg");
+        System.out.println(this.getVitorias() + " vitoria");
+        System.out.println(this.getDerrotas() + " derrotas");
+        System.out.println(this.getEmpates() + " empates");
+    }
+    public void status(){
+        
+        
+    }
+    public void ganharLuta(){
+        this.setVitorias(this.getVitorias() + 1);
+    }
+    public void perderLuta(){
+        this.setDerrotas(this.getDerrotas() + 1);
+    }
+    public void empatarLuta(){
+        this.setEmpates(this.getEmpates() +1);
+    }
+    
+    //metodos especiais
+    public Lutador(String no, String na, int id, int vi, int de, int em, 
+                    float al, float pe) {
+        this.nome = no;
+        this.nacionalidade = na;
+        this.idade = id;
+        this.vitorias = vi;
+        this.derrotas = de;
+        this.empates = em;
+        this.altura = al;
+        this.peso = pe;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    private void setCategoria() {
+        if (this.peso < 52.2) {
+            this.categoria = "Invalido!";
+        } else if (this.peso <= 70.3) {
+            this.categoria = "Leve";
+        } else if (this.peso <= 83.9) {
+            this.categoria = "Medio";
+        }else if (this.peso <= 120.2){
+            this.categoria = "Pesado";
+        } else {
+            this.categoria = "Invalido";
+        }
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+        this.setCategoria();
+    }
+    
+    
+    
+    
 }
+
